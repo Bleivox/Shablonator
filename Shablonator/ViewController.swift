a1ce8b455340f56134d5cc8a400b4ca0383be732
Bleivox <nikita-inbox@mail.ru>
2025-09-02 23:49:38 +0300

    - Удалил Storyboard и упоминания о нем
    - Добавил начальную навигацию показа первой сцены

--- Shablonator/ViewController.swift
+++ Shablonator/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Shablonator"
+        
+        let label = UILabel()
+               label.text = "Hello"
+               label.translatesAutoresizingMaskIntoConstraints = false
+
+               view.addSubview(label)
+               NSLayoutConstraint.activate([
+                   label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+               ])
     }
 
 
